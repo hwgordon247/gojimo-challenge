adac304e7022ee4aff6f490900660476ff583cc4
Henry Gordon <hwgordon247@gmail.com>
2016-04-14 14:59:08 +0100

    further refactor of data analyse class

--- lib/models/data_evaluate.rb
+++ lib/models/data_evaluate.rb
@@ -6,19 +6,21 @@ class DataEvaluate
 
   def return_qualifications
     qualification_store = []
-    @data.each do |qualification|
-      qualification_store << qualification['name']
-    end
+    @data.each { |qualification| qualification_store << qualification['name'] }
     qualification_store
   end
 
   def return_subjects(chosen_qualification)
-    subject_store = []
     subjects = select_qualification(chosen_qualification)
-    subjects.each do |subject|
-      subject_store << subject['title']
-    end
-    subject_store
+    store_subjects(subjects)
+  end
+
+
+
+  def store_subjects(subjects)
+    subject_array = []
+    subjects.each { |subject| subject_array << subject['title'] }
+    subject_array
   end
 
   def select_qualification(chosen_qualification)
